cb22146ebae9ceda3ca70c4f553aaf12defceba7
Ngô Đức Huy <ngoduchuy64@giaohangtietkiem.vn>
2021-10-22 18:11:06 +0700

    added SparkContext

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,39 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 
-class Tar {
-    @Option(names = {"-l", "--local"}, description = "")
+class EAFIMConfig {
+    @Option(names = {"-l", "--local"}, description = "local mode")
     boolean isLocal;
 
-    @Option(names = { "-i", "--input" }, paramLabel = "inputFilename", description = "")
+    @Option(names = { "-i", "--input" }, paramLabel = "inputFilename", description = "input file name")
     String inputFilename;
 
-    @Option(names = { "-mp", "--minSupPercent" }, description = "")
-    float minSupPercent = -1;
-
-    @Option(names = { "-mc", "--minSupCount" }, description = "")
+    @Option(names = { "-mc", "--minSupCount" }, description = "min sup count (in integer)")
     long minSupCount = -1;
+
+    @Override
+    public String toString() {
+        return "EAFIMConfig{" +
+                "isLocal=" + isLocal +
+                ", inputFilename='" + inputFilename + '\'' +
+                ", minSupCount=" + minSupCount +
+                '}';
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Tar tar = new Tar();
-        new CommandLine(tar).parseArgs(args);
-        System.out.println(tar.inputFilename);
-        System.out.println(tar.isLocal);
-        System.out.println(tar.minSupPercent);
-        System.out.println(tar.minSupCount);
+        EAFIMConfig eafimConfig = new EAFIMConfig();
+        new CommandLine(eafimConfig).parseArgs(args);
 
+        Logger.getLogger("org").setLevel(Level.toLevel("error"));
+        Logger.getLogger("akka").setLevel(Level.toLevel("error"));
+        SparkConf sparkConf = new SparkConf().setAppName("BIGMiner").set("spark.driver.maxResultSize", "40g").set("spark.memory.storageFraction", "0.1").set("spark.driver.cores", "4");
+        if (eafimConfig.isLocal) sparkConf.setMaster("local[*]");
+
+        System.out.println(eafimConfig);
     }
 }
