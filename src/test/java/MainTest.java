7bb02afce544a704c2848f56292a742870f5fa08
Ngô Đức Huy <ngoduchuy64@giaohangtietkiem.vn>
2021-10-27 10:32:54 +0700

    added more tests

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,26 +1,52 @@
 import org.junit.Test;
 
+import static java.lang.Math.min;
 import static org.junit.Assert.assertEquals;
 
 public class MainTest {
-    @Test
-    public void testSmall(){
-        Main.main(new String[]{"-i", "datasets/small.txt",  "-l", "-mc", "1"});
+    private void test(int[] minSupCounts, int[] outputs, String file, Integer... limit){
+        int n = min(limit.length > 0 ? limit[0] : 1000000000, minSupCounts.length);
+        for(int i = 0; i < n; i++) {
+            assertEquals(Main.main(new String[]{"-i", file, "-l", "-mc", Integer.toString(minSupCounts[i])}), outputs[i]);
+        }
     }
 
     @Test
     public void testChess(){
-        assertEquals(Main.main(new String[]{"-i", "datasets/chess.dat.txt",  "-l", "-mc", "2700"}), 3134);
+        int[] minSupCounts = new int[]{3000, 2700, 2500, 2000};
+        int[] outputs = new int[]{155, 3134, 11493, 166580};
+        test(minSupCounts, outputs, "datasets/chess.dat.txt", 3);
     }
 
     @Test
     public void testRetail(){
-        Main.main(new String[]{"-i", "datasets/retail.dat",  "-l", "-mc", "100"});
+        int[] inputs = new int[]{1000, 500};
+        int[] outputs = new int[]{135, 468};
+        test(inputs, outputs, "datasets/retail.dat");
+
+    }
+
+    @Test
+    public void testConnect(){
+        int[] minSupCounts = new int[]{66666, 66000, 60000, 59000, 58000, 40534, 30400, 27022};
+        int[] outputs = new int[]{58, 261, 41143, 69847, 109663, 21252795, 173538667, 339987447};
+        test(minSupCounts, outputs, "datasets/connect.dat", 2);
     }
 
     @Test
     public void testMushroom(){
-        Main.main(new String[]{"-i", "datasets/mushroom.dat",  "-l", "-mc", "2500"});
+        int[] inputs = new int[]{2500};
+        int[] outputs = new int[]{2365};
+        test(inputs, outputs, "datasets/mushroom.dat");
     }
 
+    /*
+    TODO: increase stack size (Stack overflow)
+    @Test
+    public void testWebdocs(){
+        int[] inputs = new int[]{400000};
+        int[] outputs = new int[]{2365};
+        test(inputs, outputs, "datasets/webdocs.dat");
+    }
+     */
 }
