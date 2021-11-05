0c4062e4c81b1fd7ec7c6c1a765ea4f8b5187832
Ngô Đức Huy <ngoduchuy64@giaohangtietkiem.vn>
2021-11-05 17:42:34 +0700

    seperate k = 1 and greater k

--- src/main/java/eafim/FrequentFinder.java
+++ src/main/java/eafim/FrequentFinder.java
@@ -1,13 +1,8 @@
 package eafim;
 
 import org.apache.spark.api.java.JavaRDD;
-import scala.Tuple2;
-import utils.ArrayUtils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 
 public class FrequentFinder {
     /**
@@ -32,36 +27,14 @@ public class FrequentFinder {
     public static int[][] findFrequents(JavaRDD<int[]> inputRdd,
                                         int[][] previousFrequent,
                                         int k,
-                                        int minSup,
-                                        HashMap<Integer, Integer> singletonOrder){
+                                        int minSup){
 
-        int[][] distinctItems = new int[0][];
-        if (k == 1){
-            List<Tuple2<Integer, Integer>> items = inputRdd.flatMap(arr -> ArrayUtils.primitiveArrayToArrayList(arr).iterator())
-                    .mapToPair(word -> new Tuple2<>(word, 1))
-                    .reduceByKey(Integer::sum)
-                    .collect();
-
-            for(Tuple2<Integer, Integer> pair: items){
-                singletonOrder.put(pair._1, pair._2);
-            }
-
-            distinctItems = new int[items.size()][];
-            int i = 0;
-            for(Tuple2<Integer, Integer> tp: items) distinctItems[i++] = new int[]{tp._1};
-            Arrays.sort(distinctItems, (x, y) -> InputRDDUpdater.ascendingSupport(x[0], y[0], singletonOrder));
-        }
-
-        int[][] candidatesLengthOne = distinctItems; // for k = 1
         JavaRDD<int[]> fm = inputRdd.mapPartitions(
                 iterator -> {
                     // EAFIM doesn't broadcast generated candidates. It only broadcasts frequent itemsets,
                     // and then it re-generates candidates in each partition.
                     int[][] generatedCandidates;
-                    if (k > 1) generatedCandidates = CandidateGenerator.gen(previousFrequent);
-                    else {
-                        generatedCandidates = candidatesLengthOne;
-                    }
+                    generatedCandidates = CandidateGenerator.gen(previousFrequent);
                     HashTree candidateTree = HashTree.build(generatedCandidates);
                     int[] supports = new int[generatedCandidates.length];
                     while (iterator.hasNext()) {
@@ -78,11 +51,8 @@ public class FrequentFinder {
         ArrayList<int[]> newFrequents = new ArrayList<>();
 
         int[][] generatedCandidates;
-        if (k > 1) generatedCandidates = CandidateGenerator.gen(previousFrequent);
-        else generatedCandidates = candidatesLengthOne;
+        generatedCandidates = CandidateGenerator.gen(previousFrequent);
 
-        //System.out.println("generated candidates: " + Arrays.deepToString(generatedCandidates));
-        //System.out.println("corresponding supports: " + Arrays.toString(supports));
         for(int i = 0; i < supports.length; i++) if (supports[i] >= minSup) {
             newFrequents.add(generatedCandidates[i]);
         }
